0edd93e4939390a82c7de9a958a797d23c62e893
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-22 03:34:13 +0000

    [Add]解説より修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   
 
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home/about" => "homes#about"
 
