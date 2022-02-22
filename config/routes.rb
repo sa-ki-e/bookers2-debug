8c9839c2b54e90a8780e56230c54e7cea9fe3c1e
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-22 22:54:44 +0000

    [Add]いいね機能コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
+  get "home/about" => "homes#about"
   
 
   devise_for :users
   resources :users, only: [:index,:show,:edit,:update]
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "home/about" => "homes#about"
 
 end
