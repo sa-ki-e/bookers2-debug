922b665ae7a719f121591c21521a5ec6917052ac
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-24 02:07:52 +0000

    [Add]フォロー機能ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
   get "home/about" => "homes#about"
 
-  devise_for :users
+  devise_for :users do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
+  end
+  
   resources :users, only: [:index,:show,:edit,:update]
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
