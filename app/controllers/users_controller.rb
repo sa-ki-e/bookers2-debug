2edad7937deebc4f88b3a0f6e666ffe0136bf03f
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-25 05:52:37 +0000

    [中断]パスのエラー未解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   before_action :authenticate_user, only: [:edit, :update]
+   before_action :authenticate_user!, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
