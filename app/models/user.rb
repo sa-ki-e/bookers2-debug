a2089aeb1d119c829f48be7d434215fbdf5b924b
Sakie Kawamichi <sakie0313@gmail.com>
2022-03-08 07:14:47 +0000

    [中断]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
+  has_many :group_users
+  has_many :groups, through: group_users
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
